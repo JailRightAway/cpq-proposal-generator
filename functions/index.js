fac1e6676877329d187e29ddbbe727034bfd958e
Stephen Huang <stephen.huang@meridianlink.com>
2026-08-28 12:51:15 -0400

    add static file serving

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,7 @@ const app = express();
 // Middleware
 app.use(cors({ origin: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 console.log('Importing routes...');
 
